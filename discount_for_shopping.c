3dd36bad6edbe0288802aa60234454388906a679
Bharath1137 <103872274+Bharath1137@users.noreply.github.com>
2022-04-23 18:14:50 +0530

    Create discount_for_shopping.c

---
+++ discount_for_shopping.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+	
+	double quantity,price,amount,discount;
+	
+	printf("Enter Quantity and Price:");
+	scanf("%lf %lf",&quantity, &price);
+
+	amount=quantity*price;
+
+	if(amount>5000)
+
+	{
+
+		discount=amount*0.05;
+
+		amount=amount-discount;
+
+	}
+	
+	printf("%lf",amount);
+	
+	return 0;
+
+}
+/* GUNDA BHARATH SUNDAR 
+RA2111003011668*/
+
+				
+			
